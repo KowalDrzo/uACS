31ec5390e23501dfc7efdc177fe1604b28b38439
Michał Kowalski <248959@student.pwr.edu.pl>
2025-06-22 20:30:02 +0200

    Add filtering

--- ESP_uACS/src/main.cpp
+++ ESP_uACS/src/main.cpp
@@ -10,6 +10,7 @@
 
 #include "pinout.h"
 #include "pid.h"
+#include "filter.h"
 
 Adafruit_BMP085 bmp;
 ITG3200 gyro;
@@ -20,10 +21,9 @@ Adafruit_ADXL345_Unified accel;
 Servo servo1, servo2;
 
 PID pid(0.2, 0.001, 0.001);
-
-/*float Setpoint = 0, Input, Output;
-float Kp=2, Ki=5, Kd=1;
-PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);*/
+Filter filterX(0.2);
+Filter filterY(0.2);
+Filter filterZ(0.2);
 
 void setup() {
 
@@ -49,22 +49,30 @@ void setup() {
 
     servo1.write(90);
     servo2.write(90);
-
-    //myPID.SetMode(AUTOMATIC);
 }
 
 void loop() {
 
     //Serial.println(bmp.readPressure());
 
-    gyro.getRotation(&gx, &gy, &gz);
+    float tableX[3];
+    float tableY[3];
+    float tableZ[3];
+
+    for (int8_t i = 0; i < 3; i++) {
+        gyro.getRotation(&gx, &gy, &gz);
+        tableX[i] = gx;
+        tableY[i] = gy;
+        tableZ[i] = gz;
+    }
+
+    // Filtrowanie:
+    gx = filterX.doFiltering3Vals(tableX);
+    gy = filterY.doFiltering3Vals(tableY);
+    gz = filterZ.doFiltering3Vals(tableZ);
+
     if (gy > -100 && gy < 100) gy = 0;
-    //Serial.printf("%0.2f;%0.2f;%0.2f\n", gx / 14.375, gy / 14.375, gz / 14.375);
-    /*Input = gy / 14.375;
-    myPID.Compute();
-    Serial.println(Output);
-    servo1.write(90 + (Output - 127) / 25);
-    servo2.write(90 + (Output - 127) / 25);*/
+    Serial.printf("%0.2f;%0.2f;%0.2f\n", gx / 14.375, gy / 14.375, gz / 14.375);
 
     float setpoint = 0.0;  // Wartość zadana
     float actual = gy / 14.375;     // Wartość aktualna
@@ -76,7 +84,7 @@ void loop() {
     int angle = 90 + control;
     if (angle > 105) angle = 105;
     else if (angle < 75) angle = 75;
-    Serial.println(angle);
+    //Serial.println(angle);
 
     servo1.write(angle);
     servo2.write(angle);
